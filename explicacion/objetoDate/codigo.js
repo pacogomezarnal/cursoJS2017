c09d7b48533e0666cd770b0aac7ddc5b098fdb76
Francisco José Gómez Arnal <fjgomez@florida-uni.es>
2017-10-24 12:12:42 +0200

    Ejemplos en aula

---
+++ explicacion/objetoDate/codigo.js
@@ -0,0 +1,25 @@
+//Inicializacion de fechas
+var fechaHoy=new Date();
+var fechaCumplePaco=new Date(2018,1,25);
+
+//Muestro el dia de hoy
+console.log(fechaHoy);
+console.log(fechaCumplePaco);
+console.log("Paco cumple el dia: "+fechaCumplePaco.getDate());
+
+//Dia de la semana
+console.log("Dia de la semana: "+fechaCumplePaco.getDay());
+switch (fechaCumplePaco.getDay()) {
+  case 0:
+    console.log("Dia de la semana: Domingo");
+    break;
+  case 1:
+    console.log("Dia de la semana: Lunes");
+    break;
+  case 2:
+    console.log("Dia de la semana: Martes");
+    break;
+  default:
+    console.log("Dia de la semana: ERROR");
+    break;
+}
